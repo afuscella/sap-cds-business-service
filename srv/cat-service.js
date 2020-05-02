65977d0141b39490e89fb1f05ee055982076e389
Arthur Silva <fuscellaarthur@gmail.com>
2020-05-01 23:47:34 -0300

    added business logic

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,17 +1,70 @@
-// module.exports = (srv) => {
-    
-//     // mock data for Books entity
-//     srv.on ('READ', 'Books', () =>[
-//         { ID: 181, title: "Designing Great Beers", author_ID: 10000001, stock: 10 },
-//         { ID: 203, title: "The Homebrew Handbook", author_ID: 10000002, stock: 52 },
-//         { ID: 204, title: "The Pocket Homebrew Handbook", author_ID: 10000002, stock: 138 },
-//         { ID: 542, title: "Small Brewery Finance", author_ID: 10000004, stock: 5 }
-//     ])
+/**
+ * Implementation for CatalogService defined in ./cat-service.cds
+ */
+module.exports = (srv) => {
 
-//         // mock data for Authors entity
-//     srv.on('READ', 'Authors', () =>[
-//         { ID: 10000001, name: "Ray Daniels" },
-//         { ID: 10000002, name: "Dave Law" },
-//         { ID: 10000004, name: "Maria Pearman" }
-//     ])
-// }
+    const {Books} = cds.entities;
+
+    srv.after('READ', 'Books', (each) => {
+        if (each.stock > 15) {
+            each.title += ' -- 11% discount!';
+        }
+    })
+
+    srv.before('CREATE', 'Orders', async (req) =>{
+        const tx = cds.transaction(req), order = req.data;
+        order.Total = 0;
+
+        if (order.Items) {
+            const affectedRows = await updateStock(tx, order.Items);
+
+            if (affectedRows.some(row => row)) {
+                for (item of order.Items) {
+                    order.Total += await calculateTotal(tx, item);
+                }
+
+            } else {
+                req.error(409, 'Sold out, sorry') 
+            } 
+        }
+    })
+
+    /**
+     * 
+     * @param {*} tx 
+     * @param {*} items 
+     */
+    async function updateStock(tx, items) {
+        return await tx.run(items.map(item =>
+            UPDATE(Books) 
+                .where({ID: item.book_ID})
+                .and(`stock >=`, item.amount)
+                .set(`stock -=`, item.amount)
+            )
+        )
+    }
+
+    /**
+     * 
+     * @param {*} tx 
+     * @param {*} item 
+     */
+    async function calculateTotal(tx, item) {
+        return await 
+            tx.run( 
+                SELECT.from(Books).where({ID: item.book_ID})
+            ).then(function (books) {
+                let value = books.map(data => { return formatTotal(data.price, item.amount) });
+                return parseFloat(value);
+        })
+    }
+
+    /**
+     * 
+     * @param {*} price 
+     * @param {*} amount 
+     */
+    function formatTotal(price, amount) {
+        return price * amount;
+    }
+}
